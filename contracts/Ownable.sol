c07c2cba487494b7bec8a1c9885099ef98c9edff
Ringaile Valiaugaite <rva@crossconsense.de>
2020-07-03 15:19:26 +0200

    first commit

---
+++ contracts/Ownable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.6.0;
+
+contract Ownable {
+    address private _owner;
+    
+    constructor() public {
+        _owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(isOwner(), "You are not the owner.");
+        _;
+    }
+    
+    function isOwner() public view returns(bool) {
+        return(msg.sender == _owner);
+    }
+}
